b897647f21762a02f7da1f2b9b63be7475cd1065
Issa Abubakar <abubakarissa47722@gmail.com>
2026-05-08 12:37:24 +0100

    Initial Commit

---
+++ taskily/taskilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskilyApp.swift
+//  taskily
+//
+//  Created by Issa Abubakar on 08/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct taskilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
